f3cdf9fbf440f53d795e63c67d990a2467f5eb0a
Néstor Manuel <nestor706@gmail.com>
2015-03-14 19:17:11 -0430

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Nestor'}
+    posts = [
+        {
+            'author': {'nickname': 'Jhon'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
