a664353cdc2432fb30321b9ee5feb10942176291
Néstor Manuel <nestor706@gmail.com>
2015-04-01 12:37:54 -0430

    Flask-SQLAlchemy and SQLAlchemy-migrate configuration. User model created.

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ __author__ = 'nessie'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'secret-key'
 
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
