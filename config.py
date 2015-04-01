5220ee6facf702597eea876bbe0b88fbd85f1a09
Néstor Manuel <nestor706@gmail.com>
2015-04-01 13:03:20 -0430

    Script to create the database and a directory to manage migrations.

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 __author__ = 'nessie'
 
+import os
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'secret-key'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
